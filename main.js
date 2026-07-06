8d2ce9c7ebee29ed5ff40b516f566acf801cc896
sangyong1203 <sangyong1203@trgtech.co.kr>
2026-07-06 14:38:01 +0900

    Add Windows installer build config

--- main.js
+++ main.js
@@ -75,6 +75,7 @@ function createPlayerWindow() {
     height: 540,
     show: false,
     title: APP_NAME,
+    icon: path.join(__dirname, 'assets', 'icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
